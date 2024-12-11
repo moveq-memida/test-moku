58b712c6c0c0dca860434714ac447320e41ee495
memida <moveqmemida@gmail.com>
2024-12-11 17:16:46 +0900

    各ページのレスポンシブ調整

--- src/contact-thanks.php
+++ src/contact-thanks.php
@@ -10,12 +10,21 @@ $og_description = "Habit+の公式サイトへようこそ。";
 include __DIR__ . '/components/Header.php';
 ?>
 
-<main>
+<main class="wide-wrap">
     <section class="l-thanks">
+        <div class="c-status">
+            <ul class="c-status-list">
+                <li class="c-status-item">ご入力</li>
+                <li><img src="/assets/images/contact\arw-bold.svg" class="c-status-item--arw"></li>
+                <li class="c-status-item">ご確認</li>
+                <li><img src="/assets/images/contact\arw-bold.svg" class="c-status-item--arw"></li>
+                <li class="c-status-item c-status-item--pink">送信完了</li>
+            </ul>
+        </div>
         <div class="p-thanks">
             <h2 class="p-thanks__title">お問い合わせありがとうございます</h2>
             <p class="p-thanks__txt">
-                お問い合わせ内容を送信いたしました。<br>
+                お問い合わせ内容を送信いたしました。<br class="pc-blk">
                 頂いた内容を確認後、担当者からご返答を差し上げます。<br>
                 ご返答までに2～3営業日ほど頂きますのでご了承いただければ幸いです。
             </p>
