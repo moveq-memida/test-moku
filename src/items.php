7f8094f21761707d6aa61cc160536aba59397335
memi <memiomaru@gmail.com>
2024-12-14 09:16:54 +0900

    商品紹介ページをFLOCSSに変更

--- src/items.php
+++ src/items.php
@@ -7,13 +7,13 @@ $og_image = "http://og.png";
 $og_title = "TOP |【公式】habit+";
 $og_description = "Habit+の公式サイトへようこそ。";
 
-include __DIR__ . '/components/Header.php';
+require_once 'components/header.php';
 require_once 'components/breadcrumbs.php';
 require_once 'components/status.php';
 require_once 'components/title.php';
 ?>
 
-<main class="l-main wide-wrap">
+<main class="l-main l-items wide-wrap">
     <section class="l-main__header">
         <?php renderTitle('Our items', 'アイテムご紹介'); ?>
         <?php renderBreadcrumbs([
@@ -21,82 +21,82 @@ require_once 'components/title.php';
             ['text' => 'アイテムご紹介']
         ]); ?>
     </section>
-    <section class="l-food">
+    <section class="p-food-drink">
         <div class="secttl-box">
             <h2 class="sec-ttl zenkaku">Food/Drink</h2>
             <p class="ja-ttl">フード・ドリンク</p>
         </div>
-        <div class="p-food__wrapper">
-            <ul class="c-ourItem-list">
-                <li class="c-ourItem-item">
-                    <picture>
-                        <source srcset="/assets/images/items/item-food.png" media="(max-width: 768px)" />
-                        <img src="/assets/images/items/item-food@2x.png" alt="MITASERU グリーンスムージー ミックスフルーツ味 180g(30日分)">
+        <div class="p-item-list-wrapper">
+            <ul class="p-item-list">
+                <li class="p-item">
+                    <picture class="p-item__picture">
+                        <source srcset="/assets/images/items/item-food.png 1x, /assets/images/items/item-food@2x.png 2x">
+                        <img src="/assets/images/items/item-food@2x.png" alt="MITASERU グリーンスムージー ミックスフルーツ味 180g(30日分)" class="p-item__image">
                     </picture>
-                    <span class="c-ourItem-category zenkaku">drink</span>
-                    <h3 class="c-ourItem-title">MITASERU グリーンスムージー<br>ミックスフルーツ味 180g(30日分)</h3>
-                    <p class="c-ourItem-txt">【初回限定割引・定期配送】一食14.5kcal 野菜・果物140種類以上 レタス1/2分の食物繊維 ビタミン10種 大豆イソフラボン 合成着色料不使用</p>
-                    <p class="c-ourItem-price"><span class="c-ourItem-price-dollar zenkaku">￥</span>1,980<span class="c-ourItem-price-tax">( 税込 )</span></p>
+                    <span class="p-item__category zenkaku">drink</span>
+                    <h3 class="p-item__title">MITASERU グリーンスムージー<br>ミックスフルーツ味 180g(30日分)</h3>
+                    <p class="p-item__description">【初回限定割引・定期配送】一食14.5kcal 野菜・果物140種類以上 レタス1/2分の食物繊維 ビタミン10種 大豆イソフラボン 合成着色料不使用</p>
+                    <p class="p-item__price"><span class="p-item__price-symbol zenkaku">￥</span>1,980<span class="p-item__price-tax">( 税込 )</span></p>
                 </li>
-                <li class="c-ourItem-item">
-                    <picture>
-                        <source srcset="/assets/images/items/item-food.png" media="(max-width: 768px)" />
-                        <img src="/assets/images/items/item-food@2x.png" alt="MITASERU グリーンスムージー ミックスフルーツ味 180g(30日分)">
+                <li class="p-item">
+                    <picture class="p-item__picture">
+                        <source srcset="/assets/images/items/item-food.png 1x, /assets/images/items/item-food@2x.png 2x">
+                        <img src="/assets/images/items/item-food@2x.png" alt="MITASERU グリーンスムージー ミックスフルーツ味 180g(30日分)" class="p-item__image">
                     </picture>
-                    <span class="c-ourItem-category">drink</span>
-                    <h3 class="c-ourItem-title">MITASERU グリーンスムージー<br>ミックスフルーツ味 180g(30日分)</h3>
-                    <p class="c-ourItem-txt">【初回限定割引・定期配送】一食14.5kcal 野菜・果物140種類以上 レタス1/2分の食物繊維 ビタミン10種 大豆イソフラボン 合成着色料不使用</p>
-                    <p class="c-ourItem-price"><span class="c-ourItem-price-dollar zenkaku">￥</span>1,980<span class="c-ourItem-price-tax">( 税込 )</span></p>
+                    <span class="p-item__category">drink</span>
+                    <h3 class="p-item__title">MITASERU グリーンスムージー<br>ミックスフルーツ味 180g(30日分)</h3>
+                    <p class="p-item__description">【初回限定割引・定期配送】一食14.5kcal 野菜・果物140種類以上 レタス1/2分の食物繊維 ビタミン10種 大豆イソフラボン 合成着色料不使用</p>
+                    <p class="p-item__price"><span class="p-item__price-symbol zenkaku">￥</span>1,980<span class="p-item__price-tax">( 税込 )</span></p>
                 </li>
-                <li class="c-ourItem-item c-ourItem-item-sp">
-                    <picture>
-                        <source srcset="/assets/images/items/item-food.png" media="(max-width: 768px)" />
-                        <img src="/assets/images/items/item-food@2x.png" alt="MITASERU グリーンスムージー ミックスフルーツ味 180g(30日分)">
+                <li class="p-item p-item--highlighted">
+                    <picture class="p-item__picture">
+                        <source srcset="/assets/images/items/item-food.png 1x, /assets/images/items/item-food@2x.png 2x">
+                        <img src="/assets/images/items/item-food@2x.png" alt="MITASERU グリーンスムージー ミックスフルーツ味 180g(30日分)" class="p-item__image">
                     </picture>
-                    <span class="c-ourItem-category">drink</span>
-                    <h3 class="c-ourItem-title">MITASERU グリーンスムージー<br>ミックスフルーツ味 180g(30日分)</h3>
-                    <p class="c-ourItem-txt">【初回限定割引・定期配送】一食14.5kcal 野菜・果物140種類以上 レタス1/2分の食物繊維 ビタミン10種 大豆イソフラボン 合成着色料不使用</p>
-                    <p class="c-ourItem-price"><span class="c-ourItem-price-dollar zenkaku">￥</span>1,980<span class="c-ourItem-price-tax">( 税込 )</span></p>
+                    <span class="p-item__category">drink</span>
+                    <h3 class="p-item__title">MITASERU グリーンスムージー<br>ミックスフルーツ味 180g(30日分)</h3>
+                    <p class="p-item__description">【初回限定割引・定期配送】一食14.5kcal 野菜・果物140種類以上 レタス1/2分の食物繊維 ビタミン10種 大豆イソフラボン 合成着色料不使用</p>
+                    <p class="p-item__price"><span class="p-item__price-symbol zenkaku">￥</span>1,980<span class="p-item__price-tax">( 税込 )</span></p>
                 </li>
             </ul>
         </div>
     </section>
-    <section class="l-supplement">
+    <section class="p-supplement">
         <div class="secttl-box">
             <h2 class="sec-ttl zenkaku">Supplement</h2>
             <p class="ja-ttl">サプリメント</p>
         </div>
-        <div class="p-food__wrapper">
-            <ul class="c-ourItem-list">
-                <li class="c-ourItem-item">
-                    <picture>
-                        <source srcset="/assets/images/items/item-supplement.png" media="(max-width: 768px)" />
-                        <img src="/assets/images/items/item-supplement@2x.png" alt="ナチュラルエラグスリム">
+        <div class="p-item-list-wrapper">
+            <ul class="p-item-list">
+                <li class="p-item">
+                    <picture class="p-item__picture">
+                        <source srcset="/assets/images/items/item-supplement.png 1x, /assets/images/items/item-supplement@2x.png 2x">
+                        <img src="/assets/images/items/item-supplement@2x.png" alt="ナチュラルエラグスリム" class="p-item__image">
                     </picture>
-                    <span class="c-ourItem-category zenkaku">supplement</span>
-                    <h3 class="c-ourItem-title">ナチュラルエラグスリム</h3>
-                    <p class="c-ourItem-txt">【初回限定割引・定期配送】肥満着もの方の脂肪減少をサポート BMI 体脂肪 ウエスト 内臓脂肪 血中中性脂肪 機能性関与成分エラグ酸3mg</p>
-                    <p class="c-ourItem-price"><span class="c-ourItem-price-dollar zenkaku">￥</span>6,000<span class="c-ourItem-price-tax">( 税込 )</span></p>
+                    <span class="p-item__category zenkaku">supplement</span>
+                    <h3 class="p-item__title">ナチュラルエラグスリム</h3>
+                    <p class="p-item__description">【初回限定割引・定期配送】肥満着もの方の脂肪減少をサポート BMI 体脂肪 ウエスト 内臓脂肪 血中中性脂肪 機能性関与成分エラグ酸3mg</p>
+                    <p class="p-item__price"><span class="p-item__price-symbol zenkaku">￥</span>6,000<span class="p-item__price-tax">( 税込 )</span></p>
                 </li>
-                <li class="c-ourItem-item">
-                    <picture>
-                        <source srcset="/assets/images/items/item-supplement.png" media="(max-width: 768px)" />
-                        <img src="/assets/images/items/item-supplement@2x.png" alt="ナチュラルエラグスリム">
+                <li class="p-item">
+                    <picture class="p-item__picture">
+                    <source srcset="/assets/images/items/item-supplement.png 1x, /assets/images/items/item-supplement@2x.png 2x">
+                        <img src="/assets/images/items/item-supplement@2x.png" alt="ナチュラルエラグスリム" class="p-item__image">
                     </picture>
-                    <span class="c-ourItem-category">supplement</span>
-                    <h3 class="c-ourItem-title">ナチュラルエラグスリム</h3>
-                    <p class="c-ourItem-txt">【初回限定割引・定期配送】肥満着もの方の脂肪減少をサポート BMI 体脂肪 ウエスト 内臓脂肪 血中中性脂肪 機能性関与成分エラグ酸3mg</p>
-                    <p class="c-ourItem-price"><span class="c-ourItem-price-dollar zenkaku">￥</span>6,000<span class="c-ourItem-price-tax">( 税込 )</span></p>
+                    <span class="p-item__category">supplement</span>
+                    <h3 class="p-item__title">ナチュラルエラグスリム</h3>
+                    <p class="p-item__description">【初回限定割引・定期配送】肥満着もの方の脂肪減少をサポート BMI 体脂肪 ウエスト 内臓脂肪 血中中性脂肪 機能性関与成分エラグ酸3mg</p>
+                    <p class="p-item__price"><span class="p-item__price-symbol zenkaku">￥</span>6,000<span class="p-item__price-tax">( 税込 )</span></p>
                 </li>
-                <li class="c-ourItem-item c-ourItem-item-sp">
-                    <picture>
-                        <source srcset="/assets/images/items/item-supplement.png" media="(max-width: 768px)" />
-                        <img src="/assets/images/items/item-supplement@2x.png" alt="ナチュラルエラグスリム">
+                <li class="p-item p-item--highlighted">
+                    <picture class="p-item__picture">
+                        <source srcset="/assets/images/items/item-supplement.png 1x, /assets/images/items/item-supplement@2x.png 2x">
+                        <img src="/assets/images/items/item-supplement@2x.png" alt="ナチュラルエラグスリム" class="p-item__image">
                     </picture>
-                    <span class="c-ourItem-category">supplement</span>
-                    <h3 class="c-ourItem-title">ナチュラルエラグスリム</h3>
-                    <p class="c-ourItem-txt">【初回限定割引・定期配送】肥満着もの方の脂肪減少をサポート BMI 体脂肪 ウエスト 内臓脂肪 血中中性脂肪 機能性関与成分エラグ酸3mg</p>
-                    <p class="c-ourItem-price"><span class="c-ourItem-price-dollar zenkaku">￥</span>6,000<span class="c-ourItem-price-tax">( 税込 )</span></p>
+                    <span class="p-item__category">supplement</span>
+                    <h3 class="p-item__title">ナチュラルエラグスリム</h3>
+                    <p class="p-item__description">【初回限定割引・定期配送】肥満着もの方の脂肪減少をサポート BMI 体脂肪 ウエスト 内臓脂肪 血中中性脂肪 機能性関与成分エラグ酸3mg</p>
+                    <p class="p-item__price"><span class="p-item__price-symbol zenkaku">￥</span>6,000<span class="p-item__price-tax">( 税込 )</span></p>
                 </li>
             </ul>
         </div>
@@ -104,6 +104,6 @@ require_once 'components/title.php';
 </main>
 
 <?php
-include __DIR__ . '/components/Cta.php';
-include __DIR__ . '/components/Footer.php';
+require_once 'components/cta.php';
+require_once 'components/footer.php';
 ?>
